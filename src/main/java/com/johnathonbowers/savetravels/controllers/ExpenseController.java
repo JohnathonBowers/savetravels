2d8c1b84dd4a0b43bbfe37902411085bb6c12ca9
Johnathon Bowers <johnathonbowers@gmail.com>
2023-01-04 15:32:28 -0500

    finish code

--- src/main/java/com/johnathonbowers/savetravels/controllers/ExpenseController.java
+++ src/main/java/com/johnathonbowers/savetravels/controllers/ExpenseController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.johnathonbowers.savetravels.models.Expense;
 import com.johnathonbowers.savetravels.services.ExpenseService;
